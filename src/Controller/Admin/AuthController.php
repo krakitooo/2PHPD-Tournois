dd23ba15495258b0e5a29b7f22a009460d3ac8b9
Bastien <156176236+krakitooo@users.noreply.github.com>
2025-05-08 12:46:30 +0200

    Update AuthController.php

--- src/Controller/Admin/AuthController.php
+++ src/Controller/Admin/AuthController.php
@@ -12,9 +12,7 @@ class AuthController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Get login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // Get last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('admin/login.html.twig', [
@@ -26,7 +24,6 @@ class AuthController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // This method can be blank: it will be intercepted by the logout key on your firewall
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
